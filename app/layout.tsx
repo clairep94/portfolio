ad4ef8019d739b1274719283369d370302c420ef
Claire Peng <128436909+clairep94@users.noreply.github.com>
2023-12-28 11:01:12 -0500

    adds bg

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-grey-50 text-gray-950 relative`}>
+        
+          {/* pink gradient */}
+          <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] 
+          rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+
+          {/* purple gradient */}
+          <div className="bg-[#d7d3f3] absolute top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] 
+          rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-8rem]"></div>
+        
+        {children}
+      </body>
     </html>
   )
 }
